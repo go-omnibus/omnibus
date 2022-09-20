7b14b4d1928520279fa0f28c9ebec76bd073c80f
chunice <chun@engineer.com>
2022-09-20 10:40:21 +0800

    feat(str): add StrPadLeft

--- str.go
+++ str.go
@@ -11,3 +11,16 @@ func Str2Bytes(s string) []byte {
 func Bytes2Str(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
+
+func StrPadLeft(input string, padLength int, padString string) string {
+	output := ""
+	inputLen := len(input)
+	if inputLen >= padLength {
+		return input
+	}
+	ll := padLength - inputLen
+	for i := 1; i <= ll; i = i + len(padString) {
+		output += padString
+	}
+	return output + input
+}
